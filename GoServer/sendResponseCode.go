a05e30717286766a00963735418074bc657731ec
Ronit Shmallo <ronits1@ac.sce.local>
2023-03-06 20:47:58 +0200

    After ron's comments

---
+++ GoServer/sendResponseCode.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"GoServer/protocol/protobuf"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"net"
+)
+
+func sendResponse(conn net.Conn, resp protobuf.AppResponse) {
+
+	//marshal transfer the info from the struct into a byte array
+	buffer, mErr := proto.Marshal(&resp)
+	if mErr != nil {
+		fmt.Println("Marshal error")
+	}
+	fmt.Println(buffer) //for debugging
+
+	//taking the size of the array as 4 bytes(int32)
+	sizeInByte := new(bytes.Buffer)
+	sizeOfBuffer := int32(len(buffer))
+	err := binary.Write(sizeInByte, binary.BigEndian, sizeOfBuffer)
+	if err != nil {
+		fmt.Println("Error converting")
+	}
+
+	//first sending the size and then the whole message
+	conn.Write(sizeInByte.Bytes())
+	conn.Write(buffer)
+}
